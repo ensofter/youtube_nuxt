d9d67d740c7f6cd8a59a9037e0b2313b680c144e
Romashov Aleksey <amromashov@gmail.com>
2020-08-18 01:09:27 +0300

    first commit

---
+++ layouts/error.vue
@@ -0,0 +1,24 @@
+<template>
+  <section>
+    <h1>Страница не найдена 404</h1>
+
+    <nuxt-link to="/">Home</nuxt-link>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "error"
+}
+</script>
+
+<style scoped>
+  section {
+    width: 500px;
+    margin: auto;
+    padding-top: 5rem;
+  }
+  h1 {
+    color: red;
+  }
+</style>
---
+++ pages/about.vue
@@ -0,0 +1,19 @@
+<template>
+  <section>
+    <h1>About this page</h1>
+
+    <p>It's my page</p>
+
+    <p>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..</p>
+  </section>
+</template>
+
+<script>
+export default {
+  middleware: ['auth']
+}
+</script>
+
+<style scoped>
+
+</style>
